af78a187c95f1d5550f3ee59cba4632875722254
yarkhan1 <153157808+yarkhan1@users.noreply.github.com>
2024-02-21 18:22:22 +0500

    Add files via upload

---
+++ javascript/first.js
@@ -0,0 +1,103 @@
+function calculateChickenPrice() {
+    let weight = parseFloat(prompt("Kitnay gram chicken lena hai?"));
+    let pricePerKg = 500;
+    let price = (weight / 1000) * pricePerKg;
+     document.write(`<h1>  Aapko ${weight} gram chicken ke liye ${price} rupay denay honge. </h1> `);
+}
+
+calculateChickenPrice();
+ //  alert("yar khanmasood ");
+//   alert("masood khan")
+//   let name = "yar khan";
+//  let father ="ramajan";
+//  let age = 23 ;
+//  console.log(name);
+//  console.log(father);
+//  console.log(age);
+
+// const project = {
+
+//       username : "yar khan masood",
+//      isfollow: true,
+//      following: 2323,
+//      follower: 1235,
+     
+
+
+// };
+
+// project["username"] = ["namamamam"];
+
+// console.log("this is for massagge");
+// console.warn("this is for warning ");
+// console.error("this is for error");
+// console.clear();
+
+//  fullname = "yar khan";
+//  lastname = "yar muhammad";
+//  age = 123;
+  
+//     document.write(`  ${fullname}   yar khan is a good boy, ${lastname} amazing boy, his age is${age},`)
+
+// window.alert("check your network")
+// window.confirm("have you check")
+// prompt("enter your name")
+// prompt("enter youe email ")
+// tab= prompt("enter you want table");
+ 
+// document.write(` <h1>  ${tab} x 1 = ${tab*1 }  </h1>`) 
+// document.write(` <h1> ${tab} x 2 = ${tab*2 }  </h1>`) 
+// document.write(` <h1> ${tab} x 3 = ${tab*3 }  </h1>`) 
+// document.write(` <h1> ${tab} x 4 = ${tab*4 }  </h1>`) 
+// document.write(` <h1> ${tab} x 5 = ${tab*5 }  </h1>`) 
+// document.write(` <h1> ${tab} x 6 = ${tab*6 }  </h1>`) 
+// document.write(` <h1> ${tab} x 7 = ${tab*7 }  </h1>`) 
+// document.write(` <h1> ${tab} x 8 = ${tab*8 }  </h1>`) 
+// document.write(` <h1> ${tab} x 9 = ${tab*9 }  </h1>`) 
+// document.write(` <h1> ${tab} x 10 = ${tab*10 }  </h1>`) 
+// document.write(` <h1> ${tab} x 11 = ${tab*11 }  </h1>`) 
+// document.write(` <h1> ${tab} x 12 = ${tab*12 }  </h1>`) 
+
+ 
+//  const profile = {
+     
+//        username :"yar muhammmad masood",
+//        title    : "masood of janobe pastoon kwah",
+//        following :  12345,
+//        follower : 12312312,
+//        isfollow : false,
+
+
+//  };
+
+//  console.log(profile);
+//  console.log(typeof profile["isfollow"]);
+
+
+
+ 
+ 
+//   document.write(prompt(" what you wont"))
+
+  
+     
+  
+   
+//  console.log(prompt (2*2) )
+// tab= prompt("enter you want table");
+// tab= prompt("enter you want table");
+// count= prompt(   );
+
+    
+//  document.write(` <h1> ${tab} x count = ${tab*count}  </h1>`) 
+
+// let a = 10
+// let b = 20
+
+//  console.log("a + b =",  a + b );
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b );
+// console.log("a / b = ", a / b);
+// console.log("a =", a, ("& b = ", b))
+
+ 
